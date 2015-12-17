26d8ea5962232f8a376234f142177f234b07c7af
jakenotjacob <jake.campbell91@gmail.com>
2015-12-17 10:43:31 -0700

    Create a supervisor for portal doors.

--- Elixir/portal/lib/portal.ex
+++ Elixir/portal/lib/portal.ex
@@ -1,21 +1,24 @@
 defmodule Portal do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3]),
+      worker(Portal.Door, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  @doc """
+  Shoots a new door with the given `color`.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
+  end
+
   defstruct [:left, :right]
   @doc """
   Starts transferring `data` from `left` to `right`.
@@ -51,7 +54,7 @@ defimpl Inspect, for: Portal do
     right_door = inspect(right)
 
     left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
-    right_data = insspect(Portal.Door.get(right))
+    right_data = inspect(Portal.Door.get(right))
 
     max = max(String.length(left_door), String.length(left_data))
 
