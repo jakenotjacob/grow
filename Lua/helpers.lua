d1a04ba63351fdca0df2f532a97006972ad97630
Jake Campbell <jake.campbell91@gmail.com>
2019-04-18 13:56:09 -0600

    Add String Splitter 9000

---
+++ Lua/helpers.lua
@@ -0,0 +1,18 @@
+--Stringsplitter 9000
+
+function string:split(...)
+  local splt = {}
+  local sep = sep or (... and "[^"..(...).."]+") or "%w+"
+  for w in string.gmatch(self,sep) do table.insert(splt,w) end
+  return splt
+end
+
+--Ex:
+
+--local x = "FOOBAR X-1 RABOOF"
+
+--{'FOOBAR X', '1 RABOOF'}
+--x:split("-")
+
+--{'FOOBAR', 'X', '1', 'RABOOF'}
+--x:split()
