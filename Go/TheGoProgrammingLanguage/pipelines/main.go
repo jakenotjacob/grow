32443521f28347c3c369c616540727a1c8302ee5
Jake Campbell <jake.campbell91@gmail.com>
2020-12-27 15:57:10 -0700

    Use unidirectional channels

--- Go/TheGoProgrammingLanguage/pipelines/main.go
+++ Go/TheGoProgrammingLanguage/pipelines/main.go
@@ -2,32 +2,31 @@ package main
 
 import "fmt"
 
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
 func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
 
-	// Counter
-	go func() {
-		defer close(naturals)
-		for x := 0; x < 100; x++ {
-			naturals <- x
-		}
-	}()
-
-	// Squarer
-	go func() {
-		defer close(squares)
-		for {
-			x, ok := <-naturals
-			if !ok {
-				break // Channel was closed and drained
-			}
-			squares <- x * x
-		}
-	}()
-
-	// Printer
-	for x := range squares {
-		fmt.Println(x)
-	}
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
 }
