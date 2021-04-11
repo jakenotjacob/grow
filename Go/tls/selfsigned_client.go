e5afb1c6cea94eeb3e4db9ce84b252d8fbddff0b
Jake Campbell <jcampbell@twilio.com>
2021-04-10 21:26:04 -0600

    Add client to connect to selfsigned ca server

---
+++ Go/tls/selfsigned_client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	certPool := x509.NewCertPool()
+
+	file, err := os.Open("cert.pem")
+	if err != nil {
+		log.Fatalf("failed to open certificate file for reading: %v", err)
+	}
+	pemCert, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("failed to read certfile: %v", err)
+	}
+
+	ok := certPool.AppendCertsFromPEM(pemCert)
+	if !ok {
+		log.Fatalf("client: failed to parse cert!")
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: certPool,
+			},
+		},
+	}
+
+	resp, err := client.Get("https://localhost:8765")
+	if err != nil {
+		log.Fatalf("client: failed to dail server: %v", err)
+	}
+	defer resp.Body.Close()
+
+	html, err := io.ReadAll(resp.Body)
+	fmt.Println(string(html))
+}
